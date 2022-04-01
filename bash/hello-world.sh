95502047290a8ce639c0eef01515263814af8199
Yogendra Pratap Singh <yogendra.pratap@opstree.com>
2022-04-01 17:13:05 +0530

    hello-world file added

---
+++ bash/hello-world.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# This file is used to show some use cases of echo command or how to print something in bash
+echo "Hello World"
+echo "Printing text without new line"
+
+echo -e "Yogendra \nPratap \nSingh"
+echo -e "Yogendra \bPratap \bSingh"
+echo -e "Yogendra \tPratap \tSingh"
+echo -e "Yogendra \vPratap \vSingh"
+echo -e "Yogendra \rPratap Singh"
+echo *
+age=25
+echo "This is your age $age"
