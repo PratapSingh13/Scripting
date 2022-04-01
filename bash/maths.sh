fd86702818e3578f5b5ddaafd7af3177636bd981
Yogendra Pratap Singh <yogendra.pratap@opstree.com>
2022-04-01 17:22:18 +0530

    maths calculation file added

---
+++ bash/maths.sh
@@ -0,0 +1,19 @@
+#/bin/bash
+
+# This script is used to perform all basic calculation of Maths
+
+# Addition
+((sum=10+20))
+echo "The sum of 10 & 20 is: $sum"
+
+# Subtraction
+((subtraction=30-20))
+echo "The subtraction of 20 from 30 is: $subtraction"
+
+# Multiplication
+((area=20*20))
+echo "Area of Square whose side is 20=$area"
+
+# Divide
+((divide=50/2))
+echo "Division of 50 by 2 is: $divide"
