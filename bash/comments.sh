9b3b0ebf86332cff07f9ddc10c27cb12d2e6b42c
Yogendra Pratap Singh <yogendra.pratap@opstree.com>
2022-04-01 17:13:15 +0530

    comment file added

---
+++ bash/comments.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# This file is used to show how to give single-line comments and multi-line comments in bash
+
+# Single line comment can be give by using a single '#'
+
+echo "Single line comment can be give by using a single #"
+
+: '
+Multi line comment can be give by using 
+: '----------'
+' 
+echo "Multi line comment can be give by using : '----------'"
